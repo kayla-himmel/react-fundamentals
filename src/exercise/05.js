01936b6800a41bb80847cb7c2c954fef7a0aa349
Kayla Himmelberger <kaylahimmelberger@MacBook-Pro.local>
2021-04-10 15:17:03 -0400

    moved completed exercises to forked repo:

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,11 +12,49 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = <Box size="small" style={{backgroundColor: 'lightblue'}}>small lightblue box</Box>
+const mediumBox = <Box size="medium" style={{backgroundColor: 'pink'}}>medium pink box</Box>
+const largeBox = <Box size="large" style={{backgroundColor: 'orange'}}>large orange box</Box>
+
+// Extra credit 1
+// function Box({className = '', size, style, ...otherProps}) {
+//   return <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}} {...otherProps}></div>;
+// };
+
+// Extra credit 2
+function Box({className = '', size, style, ...otherProps}) {
+  let sizeClassName = size ? `box--${size}` : '';
+  return (
+    <div className={`box ${className} ${sizeClassName}`} style={{fontStyle: 'italic', ...style}} {...otherProps}></div>
+  );
+};
+
+
+// function sizeSpecific() {
+//   let size;
+//   let classes;
+//   let text;
+//   let bgColor;
+
+//   if (size === "small") {
+//     classes="box--small";
+//     text="EC small lightblue box";
+//     bgColor="lightblue";
+//   } else if (size === "medium") {
+//     classes="box--medium";
+//     text="EC medium pink box";
+//     bgColor="pink";
+//   } else if (size === "large") {
+//     classes="box--large";
+//     text="EC large orange box";
+//     bgColor="orange";
+//   } else {
+//     text="EC new box without a size or color";
+//   }
+// }
 
 function App() {
+  // sizeSpecific(); 
   return (
     <div>
       {smallBox}
